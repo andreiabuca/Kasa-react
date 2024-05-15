4da5234b1dbda2f28ea483bd0941e705779146e7
Andreia Buca <andreia.buca@hotmail.com>
2024-05-15 11:23:51 +0200

    updated routing logic

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import Footer from './Components/Footer';
 import Header from './Components/Header';
+import AppRouter from './AppRouter';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <Header/>
+      <AppRouter/>
       <Footer/>
     </div>
   );
--- src/index.js
+++ src/index.js
@@ -1,22 +1,13 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
+import {createRoot} from 'react-dom/client';
 import App from './App';
+import './Style/main.scss';
 import reportWebVitals from './reportWebVitals';
-import AppRouter from './AppRouter';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+const container = document.getElementById('root');
+const root = createRoot(container);
 
-ReactDOM.render(
-  <React.StrictMode>
-    <AppRouter/>
-  </React.StrictMode>
-)
+root.render(<App/>);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
